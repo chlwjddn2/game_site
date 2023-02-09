6ccbd82295fff98a9b57fe7dd736f61ac9d7b941
chlwjddn2 <112842922+chlwjddn2@users.noreply.github.com>
2023-02-09 20:27:18 +0900

    commit

---
+++ js/main.js
@@ -0,0 +1,41 @@
+'use strict';
+const header = document.querySelector('#header');
+const main = document.querySelector('#main')
+const headerHeight = header.getBoundingClientRect().height;
+
+document.addEventListener('scroll', () => {
+    if(window.scrollY > headerHeight){
+        header.classList.add('header_dark')
+    } else{
+        header.classList.remove('header_dark')
+    }
+})
+
+const goTop = document.querySelector('.gotop');
+
+goTop.addEventListener('click', () => {
+    main.scrollIntoView({behavior: "smooth"});
+})
+
+const menu = document.querySelector('.open_menu');
+const openBtn = document.querySelector('.open_btn');
+const menuOpenBtn = document.querySelectorAll('.down_menu');
+const closeBtn = document.querySelector('.close_menu_btn');
+menuOpenBtn.forEach((item) => {
+    console.log(item);
+    item.addEventListener('click', () => {
+        menuOpenBtn.forEach((e) => {
+            e.classList.remove('show');
+        })
+        item.classList.add('show'); 
+    })
+}) 
+closeBtn.addEventListener('click', () => {
+    menu.style.transform = 'translateX(1000px)'
+    menu.style.display = 'none';
+})
+openBtn.addEventListener('click', () =>{
+    menu.style.transform = 'translateX(0)';
+    menu.style.display = 'block';
+})
+
